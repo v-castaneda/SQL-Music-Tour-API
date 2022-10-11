b35bb21d628bb7a72592adb710b9ce3338499d0b
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2022-10-10 20:41:16 -0400

    fixing class name

--- controllers/band.js
+++ controllers/band.js
@@ -3,7 +3,7 @@ const { Sequelize, DataTypes, Model } = require('sequelize')
 const sequelize = new Sequelize(process.env.PG_URI)
 
 // MODEL 
-class Bands extends Model{}
+class Band extends Model{}
 
 Band.init({
     band_id: {
