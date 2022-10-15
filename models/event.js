fc587a1c54b508565be82a104e58e6d31cf1d8f1
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2022-10-15 11:07:15 -0400

    define many-to-many in Event model

--- models/event.js
+++ models/event.js
@@ -9,8 +9,13 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      // define association here
+    static associate({ Stage, StageEvent }) {
+      // stages
+      Event.belongsToMany(Stage, {
+        foreignKey: "event_id", 
+        as: "stages", 
+        through: StageEvent
+      })
     }
   }
   Event.init({
