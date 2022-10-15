c861b16a6114b287a59d0c4ec08b7ea654971c80
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2022-10-15 11:24:56 -0400

    completed association of Band and SetTime

--- models/band.js
+++ models/band.js
@@ -9,11 +9,17 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate({ MeetGreet }) {
+    static associate({ MeetGreet, SetTime }) {
       // meet and greets
       Band.hasMany(MeetGreet, {
         foreignKey: "band_id", 
         as: "meet_greets"
+      })
+
+      // set times
+      Band.hasMany(SetTime, {
+        foreignKey: "band_id", 
+        set_times: "set_times"
       })
     }
   }
--- models/settime.js
+++ models/settime.js
@@ -9,8 +9,12 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      // define association here
+    static associate({ Band }) {
+      // band
+      SetTime.belongsTo(Band, {
+        foreignKey: "band_id", 
+        as: "band"
+      })
     }
   };
   SetTime.init({
